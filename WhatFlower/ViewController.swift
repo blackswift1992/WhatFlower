a9bc97dc44482cc96281786d7125d7ff7ed55fe6
Oleksii Moroz <blackswift1992@gmail.com>
2023-03-23 23:00:27 +0200

    add NavigationController, add cameraButton, add ImageView for taken photo

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var takenPhotoImageView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    
 
 }
 
