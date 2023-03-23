74694ef2d5122dc0a5aef66c8b89e7e7dc1fa6de
Oleksii Moroz <blackswift1992@gmail.com>
2023-03-24 00:49:53 +0200

    implement flower photo detection using CoreML and show result on the screen

--- WhatFlower/ViewController.swift
+++ WhatFlower/ViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet private weak var takenPhotoImageView: UIImageView!
@@ -23,11 +25,38 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
             takenPhotoImageView.image = image
+            
+            guard let ciimage = CIImage(image: image) else {
+                fatalError("Could not convert into CIImage")
+            }
+            
+            detect(image: ciimage)
         }
         
         imagePicker.dismiss(animated: true)
     }
     
+    private func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: MLModel(contentsOf: FlowerClassifier.urlOfModelInThisBundle)) else {
+            fatalError("Loading CoreML Model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { [weak self] (request, error) in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image.")
+            }
+            
+            self?.navigationItem.title = "\(results[0].identifier)"
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
     
     @IBAction private func cameraPressed(_ sender: UIBarButtonItem) {
         present(imagePicker , animated: true)
